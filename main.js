27330739a6b602f21d446fbe8f84d40ae18c73fc
Sanwal Iqbal <badbiz0007@gmail.com>
2024-04-19 23:58:01 +0500

    todolist ig

---
+++ main.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = []; //shoper []example
+let condition = true;
+while (condition) {
+    let todoQuestion = await inquirer.prompt([{
+            name: "firstQuestion",
+            type: "input",
+            message: "What Would You Like To add In Your Todo?",
+            validate: function (input) {
+                if (input.trim() === "") {
+                    return "Task cannot be empty.";
+                }
+                return true;
+            }
+        },
+        {
+            name: "secondQuestion",
+            type: "confirm", //when type is confirm ans in Y/N or T/F i guess
+            message: "Would You Like To Add More In Your Todo?",
+            default: "true"
+        }
+    ]);
+    todos.push(todoQuestion.firstQuestion);
+    console.log(todos);
+    condition = todoQuestion.secondQuestion;
+}
